2d8848478200f933fb8cd95322141b7459a23c2f
algreasley <al.greasley@gmail.com>
2018-03-09 16:23:43 +0000

    ES next conversion + eslint (#26)
    * add eslint, remove grunt and jshint
    
    * adjust eslintrc, rework code to near conformance, update babel config

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        "id-length": "off",
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "indent-legacy": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "line-comment-position": "off",
+        "max-lines": [
+            "error", {
+                "max": 300,
+                "skipBlankLines": true,
+                "skipComments": true
+            }
+        ],
+        "max-params": [
+            "error",
+            5
+        ],
+        "multiline-ternary": "off",
+        "no-await-in-loop": "warn",
+        "no-inline-comments": "off",
+        "no-ternary": "off",
+        "padded-blocks": [
+            "warn",
+            "never"
+        ],
+    }
+};
