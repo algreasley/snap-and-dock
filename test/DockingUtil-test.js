c96399037fcb3347fa370a6d8601c6531b6d98ae
algreasley <al.greasley@gmail.com>
2018-03-27 14:53:14 +0100

    DockingManager -> class, plus other ESnext-ing (#29)
    * move persistence service instance to DM, more esNext-ing
    
    * convert DM to class, other ESnext conversions

--- test/DockingUtil-test.js
+++ test/DockingUtil-test.js
@@ -2,10 +2,10 @@
 import assert from 'assert';
 import { intersect } from '../lib/DockingUtil';
 
-describe('DockingUtil', function() {
-    describe('intersect', function() {
-        describe('when pairs of rectangles are tested', function() {
-            it('should correctly assess whether they overlap', function() {
+describe('DockingUtil', () => {
+    describe('intersect', () => {
+        describe('when pairs of rectangles are tested', () => {
+            it('should correctly assess whether they overlap', () => {
                 assert.ok(intersect(overlappingRect1, overlappingRect2));
             });
         });
--- test/testSetup.js
+++ test/testSetup.js
@@ -17,8 +17,8 @@ global.fin = {
     }
 };
 
-function getMonitorInfoStub(callback) {
-    callback({
+function getMonitorInfoStub(successCallback) {
+    successCallback({
         primaryMonitor: {
             availableRect: {
                 left: 0,
@@ -30,3 +30,26 @@ function getMonitorInfoStub(callback) {
         nonPrimaryMonitors: []
     });
 }
+
+function getStubLocation(path) {
+    let target = global;
+    path.split('.').forEach(property => {
+        if (target[property]) {
+            target = target[property];
+        } else {
+            throw new Error('Bad property address', path);
+        }
+    });
+    return target;
+}
+
+global.replaceStubOrValue = function(path, property, stubOrValue) {
+    const target = getStubLocation(path);
+    target[`${property}_SAFE`] = target[property];
+    target[property] = stubOrValue;
+};
+
+global.resetStubOrValue = function(path, property) {
+    const target = getStubLocation(path);
+    target[property] = target[`${property}_SAFE`];
+};
