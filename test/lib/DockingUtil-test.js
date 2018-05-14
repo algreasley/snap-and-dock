af44086dfba6587fa2f64e36ac6ae7ed695b7fdf
algreasley <al.greasley@gmail.com>
2018-05-14 23:21:13 +0100

    refactoring and tests (#36)
    * DockingWindow tests - opacity
    
    * further extracting to utils and tests

--- test/lib/DockingUtil-test.js
+++ test/lib/DockingUtil-test.js
@@ -1,8 +1,9 @@
-/* globals describe, it */
+/* globals describe, it, beforeEach */
 import assert from 'assert';
-import { intersect, isInView } from '../../lib/DockingUtil';
+import { intersect, isInView, getSnapDirection } from '../../lib/DockingUtil';
 
 describe('DockingUtil', function() {
+
     describe('intersect', function() {
         describe('when pairs of rectangles are tested', function() {
             it('should correctly assess whether they overlap', function() {
@@ -10,6 +11,7 @@ describe('DockingUtil', function() {
             });
         });
     });
+
     describe('isInView', function() {
         describe('when monitor is partly off-screen', function() {
             it('should still be considered in view', function() {
@@ -17,6 +19,33 @@ describe('DockingUtil', function() {
             });
         });
     });
+
+    describe('getSnapDirection', function() {
+        let win1, win2;
+
+        describe('when the windows are side by side and within the configured range', function() {
+            beforeEach(function () {
+                win1 = {x: 100, y: 100, width: 100, height: 100, currentRange: 10};
+                win2 = {x: 205, y: 100, width: 100, height: 100, currentRange: 10};
+            });
+
+            it('should report that snapping is possible .. to the "left", from the snap target perspective', function() {
+                assert.equal(getSnapDirection(win1, win2), 'left');
+            });
+        });
+
+        describe('when the windows are side by side but overlapping horizontally beyond the configured range', function() {
+            beforeEach(function () {
+                win1 = {x: 100, y: 100, width: 100, height: 100, currentRange: 10};
+                win2 = {x: 150, y: 100, width: 100, height: 100, currentRange: 10};
+            });
+
+            it('should report that snapping is not possible', function() {
+                assert.equal(getSnapDirection(win1, win2), null);
+            });
+        });
+    });
+
 });
 
 const overlappingRect1 = {x: 0, y: 0, width: 100, height: 100};
--- test/lib/DockingWindow-test.js
+++ test/lib/DockingWindow-test.js
@@ -1,23 +1,73 @@
 /* globals beforeEach, describe, it */
+/* globals fin */
 import assert from 'assert';
 import DockingWindow from '../../lib/DockingWindow';
+import * as sinon from "sinon";
 
 const DEFAULT_WINDOW_DOCKING_OPTIONS = {};
 
 describe('DockingWindow', function() {
+    let dockingWindow;
+    let getOptionsStub;
+    let getBoundsFake, addEventListenerFake, disableFrameFake;
+    let updateOptionsSpy;
+
+    beforeEach(function () {
+        getOptionsStub = sinon.stub();
+        fin.desktop.Window.prototype.getOptions = getOptionsStub;
+        getBoundsFake = sinon.fake();
+        fin.desktop.Window.prototype.getBounds = getBoundsFake;
+        disableFrameFake = sinon.fake();
+        fin.desktop.Window.prototype.disableFrame = disableFrameFake;
+        addEventListenerFake = sinon.fake();
+        fin.desktop.Window.prototype.addEventListener = addEventListenerFake;
+        updateOptionsSpy = sinon.spy();
+        fin.desktop.Window.prototype.updateOptions = updateOptionsSpy;
+    });
 
     describe('getWindowByName', function() {
         let windows;
 
         beforeEach(function() {
-            windows = [
-                new DockingWindow({name: 'bob'}, DEFAULT_WINDOW_DOCKING_OPTIONS)
-            ]
+            dockingWindow = new DockingWindow({name: 'bob'}, DEFAULT_WINDOW_DOCKING_OPTIONS);
+            windows = [ dockingWindow ];
         });
 
         it('should retrieve the window if the name matches', function() {
-            assert.ok(DockingWindow.getWindowByName(windows, 'bob'));
+            assert.ok(DockingWindow.getWindowByName(windows, dockingWindow.name));
             assert.strictEqual(DockingWindow.getWindowByName(windows, 'dave'), undefined);
+        });
+    });
+
+    describe('opacity', function() {
+        const ORIG_OPACITY = 0.8;
+        const NEW_OPACITY = 0.4;
+
+        beforeEach(function() {
+            getOptionsStub.callsArgWith(0, {
+                opacity: ORIG_OPACITY
+            });
+            dockingWindow = new DockingWindow({name: 'bob'}, DEFAULT_WINDOW_DOCKING_OPTIONS);
+        });
+
+        describe('when the opacity value of a window is modified', function() {
+            beforeEach(function() {
+                dockingWindow.setOpacity(NEW_OPACITY);
+            });
+
+            it('the target window should have its opacity updated (via updateOptions)', function () {
+                assert.equal(updateOptionsSpy.calledWith({opacity: NEW_OPACITY}), 1);
+            });
+
+            describe('when the opacity value of a window is modified', function() {
+                beforeEach(function() {
+                    dockingWindow.resetOpacity();
+                });
+
+                it('the target window should have its opacity restored (via updateOptions)', function () {
+                    assert.equal(updateOptionsSpy.calledWith({opacity: ORIG_OPACITY}), 1);
+                });
+            });
         });
     });
 });
